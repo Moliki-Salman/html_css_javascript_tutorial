fd3e0a93786c867b379e28a387374515b5d73630
Moliki-Salman <salmanameenat@gmail.com>
2024-08-02 12:52:32 +0200

    task completed

--- cousera/script.js
+++ cousera/script.js
@@ -1,4 +1,4 @@
-function temperature () {
+function temperature(){
   //To convert celcius to farenheit
   //(CEL * 9/5) + 32
 const c = document.getElementById("celsius").value;
@@ -6,3 +6,18 @@ const f = (c * 9/5) + 32
 document.getElementById("fahrenheit").value = f
 }
 
+function weight(){
+  //To convert KGs to Pounds
+  // KG * 2.2
+  const kg = document.getElementById("kg").value;
+  const pounds = kg * 2.2;
+  document.getElementById("pounds").value = pounds;
+}
+
+function distance(){
+  //To convert kilometera to miles
+  // KM * 0.62137
+  const kilo = document.getElementById("km").value;
+  const miles = kilo * 0.62137;
+  document.getElementById("miles").value = miles;
+}
