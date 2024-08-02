ca843ff0f3193a76bf7390a5604dea4b296681ff
Moliki-Salman <salmanameenat@gmail.com>
2024-08-02 12:55:19 +0200

    modified changes in second folder

--- second_folder/server.js
+++ second_folder/server.js
@@ -6,23 +6,23 @@ const server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'html'});
 
     // send back some information
-    // res.end(`
-    //     {
-    //     "name": "Adam",
-    //     "college": "Unilag",
-    //     "occupation": "software developer"
-    // }
+    res.end(`
+        {
+        "name": "Adam",
+        "college": "Unilag",
+        "occupation": "software developer"
+    }
    
-    // `);
-    res.end (`
-    <html>
-        <body style="background: black; text-align: center; color: white;">
-            <h1>welcome</h1>
-            <h4>Training  on how to build a server with node </h4>
+    `);
+    // res.end (`
+    // <html>
+    //     <body style="background: black; text-align: center; color: white;">
+    //         <h1>welcome</h1>
+    //         <h4>Training  on how to build a server with node </h4>
 
-        </body
-    </html>
-    `)
+    //     </body
+    // </html>
+    // `)
 });
 
 server.listen(4000, "127.0.0.1");
